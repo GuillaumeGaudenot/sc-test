58d3e70eba0c57b24c0084de9acb4bbfd3ac63f4
unknown <ggaudenot@AWSOREQAMSSQL01>
2016-02-29 14:40:48 -0500

    first commit

---
+++ Tables/dbo.Products.sql
@@ -0,0 +1,8 @@
+CREATE TABLE [dbo].[Products]
+(
+[ID] [int] NULL,
+[NAME] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[DESCRIPTION] [nchar] (255) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[LINK] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+) ON [PRIMARY]
+GO
